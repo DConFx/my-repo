1faaba57698475f901e55feb7d7379d91fa27734
daniel <danielcirdei@gmail.com>
2024-09-05 16:30:39 +0100

    dockerfile uploaded

--- api/venv/Scripts/activate.bat
+++ api/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Danie\Desktop\DevOps\api\venv
+set VIRTUAL_ENV=C:\Users\Danie\my-first-repo\api\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
